c49c89d939ef5c03805d52013d1d79c899b6835d
achiyunyan <ayunyan@g.jct.ac.il>
2021-10-06 14:56:51 +0300

    Initial code- Welcome functions

---
+++ dotNet5782_4298_0184/Targil0/Program4298.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome4298();
+            Welcome0184();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome0184();
+
+        private static void Welcome4298()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
