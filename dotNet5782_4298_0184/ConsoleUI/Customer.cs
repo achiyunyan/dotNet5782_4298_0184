67b135a34b7b0d2606de5c807dcad24325491a30
achiyunyan <ayunyan@g.jct.ac.il>
2021-10-13 17:46:29 +0300

    changes

---
+++ dotNet5782_4298_0184/ConsoleUI/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class Customer
+    {
+        private int id;
+        private string name;
+        private string phone;
+        private double longitude;
+        private double lattitude;
+
+    }
+}
---
+++ dotNet5782_4298_0184/ConsoleUI/Station.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    class Station
+    {
+        private int id;
+        private int name;
+        private double longitude;
+        private double lattitude;
+    }
+}
