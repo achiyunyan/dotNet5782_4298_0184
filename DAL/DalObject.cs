14c219a9de60e51be6d63796ca3fae6e896d63dc
Zvi Korach <zvikora@gmail.com>
2021-10-14 13:06:14 +0300

    some structs

--- DAL/DalObject.cs
+++ DAL/DalObject.cs
@@ -8,14 +8,26 @@ namespace DalObject
 {
     struct Customer
     { 
-        public int id { set; get; }
-        public string name { set; get; }
-        public string phone { set; get; }
-        public double longitude { set; get; }
-        public double latitude { set; get; 
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public string Phone { set; get; }
+        public double Longitude { set; get; }
+        public double Latitude { set; get; } 
     }
     struct parcel
     {
-        
+        public int Id { set; get; }
+        public int SenderId { set; get; }
+        public int TargetId { set; get; }
+        public IDAL.DO.WeightCategories Weight { set; get; }
+        public IDAL.DO.Priorities Priority { set; get; }
+        public DateTime Requested { set; get; }
+        public int DroneId { set; get; }
+        public DateTime Scheduled { set; get; }
+        public DateTime PickedUp { set; get; }
+        public DateTime Delivered { set; get; }
     }
+    
+    struct Station
+
 }
