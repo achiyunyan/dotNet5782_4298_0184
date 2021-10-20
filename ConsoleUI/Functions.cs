4431842c45c3c4a98843cd56127e8fcd4b534515
achiyunyan <ayunyan@g.jct.ac.il>
2021-10-20 13:16:14 +0300

    fuctions

--- ConsoleUI/Functions.cs
+++ ConsoleUI/Functions.cs
@@ -1,16 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
+/*using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;*/
 
 namespace ConsoleUI
 {
-    class Functions
+    partial class Program
     {
-        static public void listd()
+
+        static public void ListsPrint(int choise)
         {
-           IDAL.DO.Station.
+            bool success = new bool();
+            int tempId = new int();
+            switch (choise)
+            {
+                case 1://station
+                    Console.WriteLine("Enter the id of the station you would like to see:");
+                    success = Int32.TryParse(Console.ReadLine(), out tempId);
+                    foreach (IDAL.DO.Station target in DalObject.DataSource.Stations)
+                    {
+                        if (target.Id.Equals(tempId))
+                            Console.WriteLine(target.ToString());
+                    }
+                    break;
+                case 2://drone
+                    Console.WriteLine("Enter the id of the drone you would like to see:");
+                    success = Int32.TryParse(Console.ReadLine(), out tempId);
+                    foreach (IDAL.DO.Drone target in DalObject.DataSource.Drones)
+                    {
+                        if (target.Id.Equals(tempId))
+                            Console.WriteLine(target.ToString());
+                    }
+                    break;
+                case 3://customer
+                    Console.WriteLine("Enter the id of the customer you would like to see:");
+                    success = Int32.TryParse(Console.ReadLine(), out tempId);
+                    foreach (IDAL.DO.Customer target in DalObject.DataSource.Customer)
+                    {
+                        if (target.Id.Equals(tempId))
+                            Console.WriteLine(target.ToString());
+                    }
+                    break;
+                case 4://parcel
+                    Console.WriteLine("Enter the id of the parcel you would like to see:");
+                    success = Int32.TryParse(Console.ReadLine(), out choise);
+                    foreach (IDAL.DO.Parcel target in DalObject.DataSource.Parcel)
+                    {
+                        if (target.Id.Equals(choise))
+                            Console.WriteLine(target.ToString());
+                    }
+                    break;
+                case 5://non affiliated parcels
+                    foreach (IDAL.DO.Parcel target in DalObject.DataSource.Parcel)
+                    {
+                        if (target.DroneId == -1)
+                            Console.WriteLine(target.ToString() + "\n");
+                    }
+                    break;
+                case 6://stations with free hubs
+                    foreach (IDAL.DO.Station target in DalObject.DataSource.Stations)
+                    {
+                        if (target.ChargeSlots > 0)
+                            Console.WriteLine(target.ToString() + "\n");
+                    }
+                    break;
+            }
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,13 +1,13 @@
-﻿using IDAL.DO;
+﻿//using IDAL.DO;
 using System;
-using System.Collections.Generic;
+/*using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;*/
 
 namespace ConsoleUI
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
@@ -47,8 +47,8 @@ namespace ConsoleUI
             int choise = new int();
             do
             {
-                choise = Convert.ToInt32(Console.ReadLine());
-                if (!(choise >= 1 && choise <= max))
+                bool success = Int32.TryParse(Console.ReadLine(), out choise);
+                if (!(choise >= 1 && choise <= max)||!success)
                 {
                     Console.WriteLine("Your number must be between 1 to {0}",max);
                 }
@@ -62,7 +62,7 @@ namespace ConsoleUI
             Console.WriteLine("1.Add a base station to the stations' list \n" +
                               "2.Add a drone to the drones' list\n" +
                               "3.Add a new customer\n" +
-                              "4.Add a package to delivery\n");
+                              "4.Add a parcel to delivery\n");
             choice = InputCheck(4);
             //add the results of choose and funcs so it will actualy do something...
         }
@@ -70,9 +70,9 @@ namespace ConsoleUI
         {
             int choice = new int();
             Console.WriteLine("Insert the number of the action you would like to commit:\n");
-            Console.WriteLine("1.Affiliating a package to a drone \n" +
-                              "2.Pick up a package with a drone\n" +
-                              "3.Suplying a package to the customer \n" +
+            Console.WriteLine("1.Affiliating a parcel to a drone \n" +
+                              "2.Pick up a parcel with a drone\n" +
+                              "3.Suplying a parcel to the customer \n" +
                               "4.Sending a drone to charge in a base station\n" +
                               "5.Releasing a drone from charging in a base station\n");
             choice = InputCheck(5);
@@ -85,7 +85,7 @@ namespace ConsoleUI
             Console.WriteLine("1.Displaying a base station \n" +
                               "2.Displaying a drone\n" +
                               "3.Displaying a customer\n" +
-                              "4.Displaying a package\n");
+                              "4.Displaying a parcel\n");
             choice = InputCheck(4);
             //add the results of choose and funcs so it will actualy do something...
         }
@@ -96,11 +96,11 @@ namespace ConsoleUI
             Console.WriteLine("1.List of base stations \n" +
                               "2.List of drones\n" +
                               "3.List of the customers \n" +
-                              "4.List of packages \n" +
-                              "5.List of non affiliated packages\n"+
+                              "4.List of parcels \n" +
+                              "5.List of non affiliated parcels\n"+
                               "6.List of stations where there are free charging hubs");
             choice = InputCheck(6);
-            //add the results of choose and funcs so it will actualy do something...
+            ListsPrint(choice);
         }
     }
 }
