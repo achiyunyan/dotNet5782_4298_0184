30441fbdc5c09b2163e4629eba7b7bf2968a0f75
Zvi Korach <zvikora@gmail.com>
2021-11-09 15:38:21 +0200

    classes finished

---
+++ BL/ListStation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
+{
+    class ListStation
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int FreeChargeSlots { set; get; }
+        public int BusyChargeSlots { set; get; }
+        public override string ToString()
+        {
+            return $" Station Id:       {Id}\n" +
+                   $" Name:             {Name}\n" +
+                   $" Free Charge slots: {FreeChargeSlots}\n" +
+                   $" Busy Charge slots: {BusyChargeSlots}\n";
+                  
+        }
+    }
+}
--- BL/Station.cs
+++ BL/Station.cs
@@ -1,11 +1,34 @@
-﻿namespace IBL.BO
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL.BO
 {
     public class Station
     {
+        private List<DroneInCharge> droneList = new List<DroneInCharge>();
+
         public int Id { set; get; }
         public string Name { set; get; }
         public Location Location { get; set; }
-        public int ChargeSlots { set; get; }
+        public int FreeChargeSlots { set; get; }
+        public List<DroneInCharge> DroneList { get { return droneList; } }
+        public override string ToString()
+        {
+            string str;
+            str = $" Station Id:       {Id}\n" +
+                  $" Name:             {Name}\n" +
+                  $" Free Charge slots:{FreeChargeSlots}\n" +
+                  $" Location:         {Location}\n";
+            if(droneList.Any())
+            {
+                str += "Drones in charge:\n\n";
+                foreach (var dr in droneList)
+                    str += dr.ToString() + '\n';
+            }
+            return str;
+        }
     }
-
 }
