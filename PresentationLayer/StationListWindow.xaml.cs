2c23833931198dd33c5a3e94787f8339e5ed5bad
achiya yunyan <ayunyan@g.jct.ac.il>
2021-12-30 20:24:17 +0200

    stations

--- PresentationLayer/StationListWindow.xaml.cs
+++ PresentationLayer/StationListWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using BO;
 namespace PL
 {
     /// <summary>
@@ -19,29 +19,27 @@ namespace PL
     /// </summary>
     public partial class StationListWindow : Window
     {
-        List<string> range=new List<string>();
-        public StationListWindow()
+        private bool exit = false;
+        private BlApi.IBL ibl;
+
+        public StationListWindow(BlApi.IBL bL)
         {
             InitializeComponent();
-
-            for (int i = 0; i < 5; i++)
-                range.Add(i.ToString());
-            comboNumberSlots.ItemsSource = range;
-        }
-
-        private void comboNumberSlots_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
+            ibl = bL;
+            lstvStations.ItemsSource = ibl.GetStationsList();
         }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-
+            if (exit == false)
+                e.Cancel = true;
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
-
+            new MainWindow().Show();
+            exit = true;
+            Close();
         }
 
         private void btnAddStation_Click(object sender, RoutedEventArgs e)
@@ -49,14 +47,32 @@ namespace PL
 
         }
 
-        private void lstvDrones_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void lstvStations_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
 
         }
 
-        private void lstvStations_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
         {
+            lstvStations.ItemsSource = ibl.GetStationsList();
+        }
 
+        private void btnGroupBySlotsNumber_Click(object sender, RoutedEventArgs e)
+        {
+            var groupsList = from Station in (IEnumerable<ListStation>)lstvStations.ItemsSource
+                             group Station by Station.FreeChargeSlots;
+            lstvStations.ItemsSource = from list in groupsList
+                                       from Station in list
+                                       select Station;
+        }
+
+        private void btnGroupByExistingSlots_Click(object sender, RoutedEventArgs e)
+        {
+            var groupsList = from Station in (IEnumerable<ListStation>)lstvStations.ItemsSource
+                             group Station by (Station.FreeChargeSlots>0);
+            lstvStations.ItemsSource = from list in groupsList
+                                     from Station in list
+                                     select Station;
         }
     }
 }
---
+++ PresentationLayer/StationWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PL
+{
+    /// <summary>
+    /// Interaction logic for StationWindow.xaml
+    /// </summary>
+    public partial class StationWindow : Window
+    {
+        public StationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void Id_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
